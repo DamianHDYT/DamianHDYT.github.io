b77fbf2a63eaa901cdc8ade22de7972e9fccd696
DamianHDYT <131803551+DamianHDYT@users.noreply.github.com>
2023-05-24 08:41:47 -0600

    Add files via upload

---
+++ Prototipo/Pagina1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>P1</title>
+</head>
+<link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Css.css">
+<body>
+<font face="Arial">
+<div id="sfcm1huqj6y2l3kaje55h5yj3p9cug4m2sg"></div>
+<script type="text/javascript" src="https://counter5.optistats.ovh/private/counter.js?c=m1huqj6y2l3kaje55h5yj3p9cug4m2sg&down=async" async></script>
+
+<link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Txtbox.css">
+<div class="messagebox"><center><br>
+	<p><Font Face="Arial Black" size=4>
+<img src="http://localhost/Prototipo/Media/AT53L1.png" width="300px">
+<center><p><br>
+Bienvenido (a) a la pagina de ArteTerapia <br> <br> <br> En esta pagina te ayudaremos con la situacion por la que estas pasando de depresion o ansiedad con el proposito de que liberes tus pensamientos negativos y busques paz mediante el Arte.
+Para iniciar da click en el boton "AYUDA" para comenzar a actuar, recuerda que la informacion es confidencial y los datos solo se usaran para establecer un contacto con nosotros o persona capacitada para atenderte de manera personal(Estimado de 3 dias).
+<br> GRACIAS POR CONFIAR EN NOSOTROS.
+		</p></center></Font>
+<br> <a href="http://localhost/Prototipo/Pagina2.php">
+	<button class="btn-5">AYUDA</button></a> <br><br>
+
+</body>
+</html>
---
+++ Prototipo/Pagina2.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>P2</title>
+</head>
+<body>
+  <link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Txtbox.css">
+</div>
+</font>
+<div class="messagebox">  
+<h1><center>¿Como te sientes?</center></h1><br><br>
+<center>
+    <p>Esto es para saber como te podemos ayudar dandole informacion que podria ayudarle</p><br><br>
+<link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Buttongod.css">
+
+<a href="http://localhost/Prototipo/Sentimiento/Ansiedad.html"><button><font color="white">Ansiedad</font></button></a>
+
+<a href="http://localhost/Prototipo/Sentimiento/Depresion.html"><button><font color="white">Depresi&oacuten</font></button></a>
+
+<a href="http://localhost/Prototipo/Sentimiento/Historias%20Reales.html"><button><font color="white">Historias Reales</font></button></a>
+
+<a href="http://localhost/Prototipo/Sentimiento/Arte%20Terapia.html"><button><font color="white">Arte Terapia</font></button></a>
+
+
+</a><br>
+</center><br> </div>
+</div>
+</div><br>
+<link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Css.css">
+<link rel="stylesheet" type="text/css" href="http://localhost/Prototipo/Css2.css">
+<div id="sfcm1huqj6y2l3kaje55h5yj3p9cug4m2sg"></div>
+<script type="text/javascript" src="https://counter5.optistats.ovh/private/counter.js?c=m1huqj6y2l3kaje55h5yj3p9cug4m2sg&down=async" async></script>
+<font face="Arial">
+<div id="sfcm1huqj6y2l3kaje55h5yj3p9cug4m2sg"></div>
+<script type="text/javascript" src="https://counter5.optistats.ovh/private/counter.js?c=m1huqj6y2l3kaje55h5yj3p9cug4m2sg&down=async" async></script>
+<div class="section">
+
+    <form class="form1" action="https://formspree.io/f/mayzajda" method="post">
+      <label for="NOMBRE" class="first-name" >NOMBRE:</label>
+      <input id="NOMBRE" type="text" name="NOMBRE" value="" required>
+
+      <label for="NUMERO" class="last-name">NUMERO:</label>
+      <input id="NUMERO" type="number" name="NUMERO" value="" required>
+
+      <label for="CIUDAD/ESTADO">CIUDAD/ESTADO:</label>
+      <input id="CIUDAD/ESTADO" type="text" name="CIUDAD/ESTADO" value="" required>
+
+      <label for="EDAD">EDAD:</label>
+      <input id="EDAD" type="number" name="EDAD" value="">
+
+      <label for="Correo">CORREO ELECTRONICO:</label>
+      <input id="CORREO ELECTRONICO" type="email" name="Correo" value="" required>
+      <br><br>
+   
+        <br><br><center>
+        <font face="Italic">Comenta tu situacion:<br><br>
+        <textarea name="comentario" rows="5" cols="50"></textarea>
+        <br><br></center>
+        <link rel="stylesheet" type="text/css" href="Radio.css">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-2.1.0.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+        <div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <br> SEXO:
+      <br>
+      <div class="btn-group btn-group-vertical" data-toggle="buttons">
+        <label class="btn active">
+          <input type="radio" name='sexo' value="Masculino" required><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i> <span>  Masculino</span>
+        </label>
+        <label class="btn active">
+          <input type="radio" name='sexo' value="Femenino" required><i class="fa fa-circle-o fa-2x"></i><i class="fa fa-dot-circle-o fa-2x"></i><span> Femenino</span>
+        </label>
+      </div>
+      <br><br>
+
+    </div>
+  </div>
+<p><font color="FD0202">*</font>La informacion solo se utilizara para una ayuda especial en caso de que<br> sea mas complicado mas la situacion.Si usted no quiere llenar el formulario puede ir arriba a <br>Como te sientes y ver como se le puede ayudar o puede utilizar el numero oficial de un psicologo<br> en la parte de abajo</p>
+       <br>
+      </div> <center><button class="custom-btn btn-10" input type="Submit">Enviar</button></a></center>
+  </div><br>
+</div>
+<br><center>
+    <img src="http://localhost/Prototipo/Media/qrcode-generado.png" width="200">
+    <p><font color="white" size="6">800-911-32-32</font></p>
+</center>
+    </form>
+<a href="http://localhost/Prototipo/Pagina1.php">
+  <center><button class="custom-btn btn-10">Regresar</button></center>   
+</a><br>
+
+</body>
+</html>
